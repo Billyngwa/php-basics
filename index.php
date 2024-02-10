0b6c39e796d4e6bd6fbfc863d129e08c944db937
Billyngwa <billytatangwa5@gmail.com>
2024-02-10 06:25:11 +0100

    added a hello world script

--- index.php
+++ index.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>My first php script</title>
 </head>
 <body>
+    <h1>My first php script </h1>
+
    <?php
         echo "Hello world!";
    ?>
