8a471b544ebfeb4bf15c8692d3f2951108928251
Billyngwa <billytatangwa5@gmail.com>
2024-02-16 16:18:55 +0100

    finished second task

--- index.php
+++ index.php
@@ -7,9 +7,26 @@
 </head>
 <body>
     <h1>My first php script </h1>
-
    <?php
         echo "Hello world!";
+
+        // Ici je cree une fonction qui prend en compte toute les taches.
+        // cette fonction est responsable de gerer les tableaux.
+
+        $numberArray = array("numberOne"=>1,"numberTwo"=>2, "numberThree"=>3,"numberFour"=>4,"numberFive"=>5, "numberSix"=>6, "numberSeven"=>7);
+
+        function weekDays(){
+
+            global $numberArray;
+            foreach($numberArray as $index => $value){
+                echo "[Key = \n " . $index . "\n"  . ", Value = " . $value . "] , ";
+            }
+
+            return ;
+
+        }
+
+        weekDays();
    ?>
 </body>
 </html>
