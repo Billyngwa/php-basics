f52c28526dac27f63d4b91e435bafb7cb6e453ba
Billyngwa <billytatangwa5@gmail.com>
2024-02-23 21:52:20 +0100

    finished tp1

--- tp1/tp1.php
+++ tp1/tp1.php
@@ -7,32 +7,66 @@
 </head>
 <body>
     <?php
-          $digits= 3;
-          $stopper = 147;
-    function waiter($timeInMillisecs){
-        $standardTime = (int)$timeInMillisecs/1000;
-        while (true) {
-          random();
-          sleep($standardTime);
-        }
-    }
+            $digits= 3;
+            $stopper = rand(pow(10, $digits-1), pow(10, $digits)-1);
+            $counter = 0;
+            $ran =  rand(pow(10, $digits-1), pow(10, $digits)-1);
 
-    function random(){
-        global $digits;
+                while ($stopper != $ran) {
+                global $counter;
+                echo "<table>
+                        <h1>Table Of No correspondence</h1>
+                        <thead>
+                            <tr>
+                                <th>N<sup>o</sup></th>
+                                <th>Generated Value</th>
+                                <th>Compared Value</th>
+                                <th>is equal</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td> $counter</td>
+                                <td> $ran</td>
+                                <td> $stopper</td>
+                                <td> false</td>
+                            </tr>
+                        </tbody>
+                    </table>", "<br>";
+                $counter++;
+                $ran =  rand(pow(10, $digits-1), pow(10, $digits)-1);
+             }
+             echo "<table>
+             <hr>
+             <h1>Table of correspondence</h1>
+             <thead>
+                 <tr>
+                     <th>N<sup>o</sup></th>
+                     <th>Generated Value</th>
+                     <th>Compared Value</th>
+                     <th>is equal</th>
+                 </tr>
+             </thead>
+             <tbody>
+                 <tr>
+                     <td> $counter</td>
+                     <td> $ran</td>
+                     <td> $stopper</td>
+                     <td> true</td>
+                 </tr>
+             </tbody>
+         </table>", "<br>";
 
-        $ran =  rand(pow(10, $digits-1), pow(10, $digits)-1);
-        echo $ran, "from random", "\n";
-        return $ran;
-    } 
-      
 
-         while ($stopper != random()) {
-                echo    random(), "\n";
-            echo "No correspondence";
-            return;
-            // $rand++;
-         }
-          
+
+          echo "Start of while loop <br>";
+
+          for($counter; $stopper!=$ran;$counter++){
+                echo "no match <br>";
+              $ran =  rand(pow(10, $digits-1), pow(10, $digits)-1);
+
+          }
+          echo "found match afer : ", $counter, " tries";
     ?>
 </body>
 </html>
